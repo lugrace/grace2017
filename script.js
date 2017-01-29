e9543bbc8a7fb69e38d28912513e5b600cd8efb5
Grace Lu <2018glu@tjhsst.edu>
2017-01-29 10:48:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 $.fn.exBounce = function(){
     var self = this;
     (function runEffect(){
-        self.effect("bounce", { times:3 }, "slow", runEffect);
+        self.effect("bounce", { times:0 }, 1000, runEffect);
     })();
    return this;
 
