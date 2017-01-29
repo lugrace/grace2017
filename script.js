c483000211fb127905abec36428377f51f8f7e24
Grace Lu <2018glu@tjhsst.edu>
2017-01-28 20:59:31 -0500

    idk

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ $(window).scroll(function(){
   }
   lastScrollTop = st;
 });
+
+var lastScrollTop=0;
+$(window).scroll(function(){
+  var st = $(this).scrollTop();
+  if(st<lastScrollTop){
+    $('#portfolio').fadeIn();
+  } else{
+    $('#portfolio').fadeOut();
+  }
+  lastScrollTop = st;
+});
